7bd015e70e75e01a2c72dadcfcffdd507ca7dd88
Danny <dannyapaza40@gmail.com>
2025-10-07 16:24:45 -0500

    Ingresar formulario

--- src/app/formulario/formulario.component.ts
+++ src/app/formulario/formulario.component.ts
@@ -1,18 +1,53 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
+import { IngresoService } from '../ingreso/ingreso.service';
+import { EgresoService } from '../egreso/egreso.service';
+import { Ingreso } from '../ingreso/ingreso.model';
 
 @Component({
   selector: 'app-formulario',
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './formulario.component.html',
-  styleUrl: './formulario.component.css'
+  styleUrl: './formulario.component.css',
 })
 export class FormularioComponent {
   tipo: string = 'ingresoOperacion';
 
-  tipoOperacion(evento : Event) {
+  //Defininimos los input del formulario
+
+  descripcionInput: string = '';
+  valorInput: number = 0;
+
+  constructor(
+    private ingresoServicio: IngresoService,
+    private egresoServicio: EgresoService
+  ) {}
+  tipoOperacion(evento: Event) {
     const elementoSelect = evento.target as HTMLSelectElement;
     this.tipo = elementoSelect.value;
+  }
+
+  agregarvalor() {
+    if (this.descripcionInput !== '' && this.valorInput > 0) {
+      if (this.tipo === 'ingresoOperacion') {
+        this.ingresoServicio.ingresos.push(
+          new Ingreso(this.descripcionInput, this.valorInput)
+        );
+      }
+      else {
+        this.egresoServicio.egresos.push(
+          new Ingreso(this.descripcionInput, this.valorInput)
+        );
+      }
+
+    }
+     else {
+        console.log("Formulario no válido");
+      }
+
+      //Limpiar el formulario
+      this.descripcionInput = '';
+      this.valorInput = 0;
   }
 }
